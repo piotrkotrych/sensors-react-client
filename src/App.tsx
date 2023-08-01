8a6ab6cf0ff97a7b384db3febf39c69fe41e40ae
Piotr Kotrych <pko@eltwin.com>
2023-08-01 09:42:34 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+//import bootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <h1>
+      test
+    </h1>
+  )
+}
+
+export default App
